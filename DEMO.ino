68b32f4cf1ac932e18b40c6e546406004ce69c01
JannickWu <31810757+JannickWu@users.noreply.github.com>
2019-04-09 15:46:36 +0200

    Update and rename PRISMA.ino to DEMO.ino

--- DEMO.ino
+++ DEMO.ino
@@ -1,14 +1,5 @@
-#include "Matrix.h"
 #include "LCD.h"
 
-//Pins of MATRIX
-#define SER_ROW 19
-#define CLK_ROW 18
-#define LATCH 17
-#define SER_COL 16
-#define CLK_COL 15
-
-MATRIX matrix(SER_ROW, CLK_ROW, SER_COL, CLK_COL, LATCH);
 
 //Pins of LCD
 
@@ -37,13 +28,6 @@ void setup() {
 }
 
 void loop() {
-
-  // put your main code here, to run repeatedly:
-      matrix.setStack(32);
-  //    matrix.setDouble(8.3);
-  //    matrix.setInteger(9);
-  //   matrix.setLevel(3.2, 7);
-  //  matrix.setText("Luohong Wu",5);
 
 
 
